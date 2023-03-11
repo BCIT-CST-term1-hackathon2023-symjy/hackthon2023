fa70fcd552687960347236e7f2057d49e9f763c9
Jeen Namkung <jnamkung@my.bcit.ca>
2023-03-11 11:44:25 -0800

    writed forum into database

---
+++ scripts/forum-write.js
@@ -0,0 +1,30 @@
+document.querySelector('form').addEventListener('submit', function(e) {
+  e.preventDefault();
+
+  var uid = null;
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      uid = user.uid;
+
+      var title = document.getElementById('title').value;
+      var category = document.getElementById('category').value;
+      var content = document.getElementById('content').value;
+
+      db.collection("posts").add({
+        title: title,
+        interest: category,
+        content: content,
+        writer: uid,
+      })
+      .then(function(docRef) {
+        console.log("Document written with ID: ", docRef.id);
+        alert("Good.");
+        location.reload();
+      })
+      .catch(function(error) {
+        console.error("Error adding document: ", error);
+        alert("Bad.");
+      });
+    }
+  });
+});
