36621ca1f437950459cdd848086922522680e491
Yongeun Kwon <ykwon35@my.bcit.ca>
2023-03-11 19:34:47 -0800

    Conclict Merging

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -4,30 +4,31 @@
 //---------------------------------------------------
 
 function loadSkeleton() {
+    $('#navbarPlaceholder').load('./text/nav.html');
     firebase.auth().onAuthStateChanged(function (user) {
         if (user) {
             // User is signed in.
             // Do something for the user here.
-            // if(document.getElementById("goForum")!=null){
-            //     document.getElementById("goForum").style.display = 'inline';
-            // }
+            document.getElementById("goForum").style.display = 'inline';
             $('#navbarPlaceholder').load('./text/nav.html');
         } else {
             // No user is signed in.
-            // if(document.getElementById("goForum")!=null){
-            //     document.getElementById("goForum").style.display = 'none';
-            // }
+            if(document.getElementById("goForum")!=null){
+                document.getElementById("goForum").style.display = 'none';
+            }
             $('#navbarPlaceholder').load('./text/nav-before-login.html');
         }
     });
 }
-loadSkeleton(); //invoke the function
+loadSkeleton(); 
 
-// function logout() {
-//     firebase.auth().signOut().then(() => {
-//         // Sign-out successful.
-//         console.log("logging out user");
-//       }).catch((error) => {
-//         // An error happened.
-//       });
-//   }
+//invoke the function
+function logout() {
+    firebase.auth().signOut().then(() => {
+        // Sign-out successful.
+        console.log("logging out user");
+        window.location.href="index.html";
+      }).catch((error) => {
+        // An error happened.
+      });
+  }
